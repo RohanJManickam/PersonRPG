b9a02ef096b17f512c9ff152463a96185e273bdb
RohanJManickam <rohanjmanickam@gmail.com>
2021-01-07 16:45:15 -0500

    Added Player Spawning And Player Movement
    Camera Follow Not Added Yet

---
+++ Person RPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody2d;
+
+    // need to use FixedUpdate for rigidbody
+    void FixedUpdate()
+    {
+    // only let the local player control the racket.
+    // don't control other player's rackets
+    if (!isLocalPlayer)
+    return;
+
+    rigidbody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+    }
+}
